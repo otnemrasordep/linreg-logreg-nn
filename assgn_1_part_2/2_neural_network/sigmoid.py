077c9871ad6afc2cfafb6d023d7badd21cf2a565
Pedro Sarmento <pedro.pereira.sarmento@gmail.com>
2020-11-07 12:12:39 +0000

    Add files via upload

---
+++ assgn_1_part_2/2_neural_network/sigmoid.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sigmoid(z):
+    
+    output = 0.0
+    #########################################
+    # Write your code here
+    # modify this to return z passed through the sigmoid function
+    
+    ########################################/
+    
+    return output
